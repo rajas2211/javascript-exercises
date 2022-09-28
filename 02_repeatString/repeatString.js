c75d813ef42afc77ab1183784c08cad32e6025a3
Rajas Shah <rajas.s.shah@gmail.com>
2022-09-28 02:47:30 -0600

    Complete first 2 exercices; problem with 3rd one

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeat) {
+    let outputString = "";
+    for (let i=1; i<=numRepeat; i++){
+        outputString += string;
+    }
+    return(outputString);
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("")
+    let resultString = "";
+    for(let i = 1; i<=stringArray.length; i++){
+        resultString += stringArray.pop();
+    }
+    return reverseString
 };
 
 // Do not edit below this line
